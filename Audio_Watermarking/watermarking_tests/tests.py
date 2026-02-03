3dfd5f436d344ba45b7005cdea78ff563358775c
BrainDBD <ioan_teodor88@yahoo.com>
2026-02-03 15:26:27 +0200

    Updated tests to include ODG measuring using AquaTK library.

--- Audio_Watermarking/watermarking_tests/tests.py
+++ Audio_Watermarking/watermarking_tests/tests.py
@@ -5,35 +5,36 @@ from Audio_Watermarking.algorithms.spread_spectrum import embed_dct_iss, extract
 from Audio_Watermarking.algorithms.qim import embed_qim, extract_qim
 from Audio_Watermarking.algorithms.qim_dither import embed_qim_dither, extract_qim_dither
 from Audio_Watermarking.algorithms.dwt_qim import embed_dwt_qim, extract_dwt_qim
+from Audio_Watermarking.algorithms.dct_dwt_svd import embed_dct_dwt_svd, extract_dct_dwt_svd
 import numpy as np
 import matplotlib.pyplot as plt
 import os, warnings
 from aquatk.metrics.PEAQ import peaq
 
 if __name__ == "__main__":
-    base_dir = 'Audio_Watermarking/sound_files/soundtrack'
+    base_dir = 'Audio_Watermarking/sound_files/active_dataset'
     watermark = 'I am Ice Cube and this is my Ice Cubical and I am here to ensure your cubes!'
     watermark_bits = text_to_bits(watermark)
 
     warnings.filterwarnings("ignore", category=UserWarning)
     
     best_peaq = -np.inf
-    for alphas in [2.5, 5.0, 10.0]:
+    for delta in [10, 25, 50, 100, 200]:
         peak_avg = 0
         for file in os.listdir(base_dir):
-            if file.startswith('watermarked_ss'):
+            if file.startswith('watermarked_dct_dwt_svd'):
                 continue
-            embed_dct_iss(os.path.join(base_dir, file), 
-                      os.path.join(base_dir, f'watermarked_ss_{alphas}_{file}'),
-                        watermark_bits, alpha=alphas)
+            embed_dct_dwt_svd(os.path.join(base_dir, file), 
+                      os.path.join(base_dir, f'watermarked_dct_dwt_svd_{delta}_{file}'),
+                        watermark_bits, step_size=delta)
             peaq_score = peaq(os.path.join(base_dir, file), 
-                                    os.path.join(base_dir, f'watermarked_ss_{alphas}_{file}'))
+                                    os.path.join(base_dir, f'watermarked_dct_dwt_svd_{delta}_{file}'))
             peak_avg += peaq_score.odg
             print(peaq_score.odg)
-        peak_avg /= len([file for file in os.listdir(base_dir) if not file.startswith('watermarked_ss')])
-        print(f"Average PEAQ for alpha {alphas}: {peak_avg}")
+        peak_avg /= len([file for file in os.listdir(base_dir) if not file.startswith('watermarked_dct_dwt_svd')])
+        print(f"Average PEAQ for delta {delta}: {peak_avg}")
         if peak_avg > best_peaq:
             best_peaq = peak_avg
-            best_alpha = alphas
+            best_delta = delta
         
-    print(f"Best alpha: {best_alpha} with PEAQ: {best_peaq}")        
+    print(f"Best delta: {best_delta} with PEAQ: {best_peaq}")        
